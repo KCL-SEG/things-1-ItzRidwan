6c47c583b5b9d1558e0b5cc7a06a8f4a6aaad0d2
Ridwan Hussein <ridwan.hussein@kcl.ac.uk>
2022-10-15 15:07:48 +0100

    Fix spelling mistake

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Things(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length = 50)
     description = models.CharField(max_length = 250)
     quantity = models.IntegerField()
